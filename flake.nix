4f090dd5e626debce80e20a0b97f007bb2169312
Nicholas Malcolm <bubylou@pm.me>
2026-03-26 20:44:47 -0400

    add app outputs and formatters

--- flake.nix
+++ flake.nix
@@ -4,9 +4,27 @@
   outputs = {conflake, ...} @ inputs:
     conflake ./. {
       inherit inputs;
+
+      apps = {
+        autopulse,
+        vykar,
+        ...
+      }: {
+        autopulse = "${autopulse}/bin/autopulse";
+        vykar = "${vykar}/bin/vykar";
+        vykar-gui = "${vykar}/bin/vykar-gui";
+        vykar-server = "${vykar}/bin/vykar-server";
+      };
+
       checks = {
         autopulse = pkgs: pkgs.autopulse;
         vykar = pkgs: pkgs.vykar;
+      };
+
+      devShell.packages = pkgs: [pkgs.rustfmt pkgs.nixfmt];
+      formatters = {
+        "*.nix" = "nixfmt";
+        "*.rs" = "rustfmt";
       };
     };
 
