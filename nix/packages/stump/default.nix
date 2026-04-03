e7d2c761bda8a16c0ef8a00b4f83c0c372953248
Nicholas Malcolm <bubylou@pm.me>
2026-04-03 12:17:43 -0400

    stump: init at v0.1.0

---
+++ nix/packages/stump/default.nix
@@ -0,0 +1,82 @@
+{
+  yarnConfigHook,
+  stdenv,
+  rustPlatform,
+  pkg-config,
+  openssl,
+  nodejs,
+  makeWrapper,
+  lib,
+  fetchYarnDeps,
+  fetchFromGitHub,
+}: let
+  pname = "stump";
+  version = "v0.1.0";
+  cargoHash = "sha256-qcNA4u3sjHTJnvA3KUfjEuYxjhv6tGYg85dZiiDUJPc=";
+  gitHash = "sha256-FavhqSckX/d3UAxLMUb3EwrNolUjZrkZNISP7GwMR58=";
+  yarnHash = "sha256-mG9kTS5bNgrjYkltAByw0kLmR1tFNSedrbkBCvDRxiA=";
+
+  src = fetchFromGitHub {
+    owner = "stumpapp";
+    repo = pname;
+    rev = version;
+    hash = gitHash;
+  };
+
+  frontend = stdenv.mkDerivation {
+    inherit pname src version;
+
+    yarnBuildScript = "web build";
+    yarnOfflineCache = fetchYarnDeps {
+      yarnLock = src + "/yarn.lock";
+      hash = yarnHash;
+    };
+
+    nativeBuildInputs = [
+      yarnConfigHook
+      nodejs
+    ];
+
+    installPhase = ''
+      mv apps/web/dist $out
+    '';
+  };
+
+  backend = rustPlatform.buildRustPackage {
+    inherit pname src version cargoHash;
+
+    preConfigure = ''
+      export GIT_REV=${lib.substring 0 8 version}
+    '';
+
+    nativeBuildInputs = [
+      pkg-config
+      makeWrapper
+    ];
+
+    buildInputs = [
+      openssl
+    ];
+
+    buildAndTestSubdir = "apps/server";
+  };
+in
+  stdenv.mkDerivation {
+    inherit pname src version;
+
+    nativeBuildInputs = [
+      makeWrapper
+    ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      makeWrapper ${backend}/bin/stump_server $out/bin/stump \
+        --set STUMP_CLIENT_DIR ${frontend}
+    '';
+
+    meta = {
+      description = "A free and open source comics, manga and digital book server";
+      homepage = "https://stumpapp.dev";
+      license = lib.licenses.mit;
+    };
+  }
