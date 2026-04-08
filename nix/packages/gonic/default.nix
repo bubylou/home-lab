ce1124052a8973569c6f6cdcd83aaf3484be7069
Nicholas Malcolm <bubylou@pm.me>
2026-04-08 12:07:07 -0400

    gonic: add ldap support
    currently in pr

--- nix/packages/gonic/default.nix
+++ nix/packages/gonic/default.nix
@@ -7,12 +7,12 @@
   buildGoModule,
 }: let
   pname = "gonic";
-  version = "v0.20.1";
-  gitHash = "sha256-tW+GuNMMeiPf5XInR1BN2L7ommWh1SPClbRew8/2+cA=";
-  vendorHash = "sha256-ynoIR4S02v3qec7447feuu/igFWR20VQVbL0GbTBpqM=";
+  version = "9c258bd";
+  gitHash = "sha256-bysoCeKTu0giPZaoGzFWaoKwqPVZQ35s9XeHtltcL1c=";
+  vendorHash = "sha256-a7cg2JSb/nMipUgTZUj3R8rQjtsA8HxfsC7pMuG3V7o=";
 
   src = fetchFromGitHub {
-    owner = "sentriz";
+    owner = "yaemiku";
     repo = pname;
     rev = version;
     sha256 = gitHash;
