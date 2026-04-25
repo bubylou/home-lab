f494813d0790b9cf481d197bc19fcd90362d6a87
Nicholas Malcolm <bubylou@pm.me>
2026-04-24 21:50:28 -0400

    vintagestory: init at 1.22.0

--- flake.nix
+++ flake.nix
@@ -10,23 +10,39 @@
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            allowUnfreePredicate = _: true;
+          };
+        };
       in {
         packages = rec {
           default = quien;
           quien = pkgs.callPackage ./packages/quien {};
           stump = pkgs.callPackage ./packages/stump {};
+          vintagestory = pkgs.callPackage ./packages/vintagestory {};
+          vintagestory-wayland = pkgs.callPackage ./packages/vintagestory {
+            inherit (pkgs) libxkbcommon wayland;
+            x11Support = false;
+            waylandSupport = true;
+          };
         };
 
         checks = {
           inherit (self.packages.${system}) quien;
           inherit (self.packages.${system}) stump;
+          inherit (self.packages.${system}) vintagestory;
+          inherit (self.packages.${system}) vintagestory-wayland;
         };
 
         apps = rec {
           default = quien;
           quien = flake-utils.lib.mkApp {drv = self.packages.${system}.quien;};
           stump = flake-utils.lib.mkApp {drv = self.packages.${system}.stump;};
+          vintagestory = flake-utils.lib.mkApp {drv = self.packages.${system}.vintagestory;};
+          vintagestory-wayland = flake-utils.lib.mkApp {drv = self.packages.${system}.vintagestory-wayland;};
         };
       }
     );
