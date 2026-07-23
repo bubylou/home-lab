0e163a4781f369818b5a165451bdc18d18d2162e
bubylou <bubylou@pm.me>
2026-07-23 12:07:11 -0400

    add build checks

--- packages/default.nix
+++ packages/default.nix
@@ -1,8 +1,13 @@
 { pkgs, ... }:
-{
+rec {
   packages = rec {
     default = vintagestory;
     vintagestory = pkgs.callPackage ./vintagestory { };
     vintagestory-server = pkgs.callPackage ./vintagestory/server-only.nix { };
+  };
+
+  checks = {
+    vintagestory = packages.vintagestory;
+    vintagestory-server = packages.vintagestory-server;
   };
 }
