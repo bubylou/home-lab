68551bd09ddde4eecea5a5a52fd9246b15aa7f64
Nicholas Malcolm <bubylou@pm.me>
2026-07-31 00:12:03 -0400

    caddy proxy

--- modules/arr/common.nix
+++ modules/arr/common.nix
@@ -11,6 +11,7 @@ in
 {
   options.home-lab.${name} = {
     enable = lib.mkEnableOption "enables ${name} service";
+    enableProxy = lib.mkEnableOption "enables caddy reverse proxy";
 
     address = lib.mkOption {
       type = lib.types.str;
@@ -23,15 +24,31 @@ in
       default = port;
       example = 443;
     };
+
+    url = lib.mkOption {
+      type = lib.types.str;
+      default = "${name}.${config.home-lab.domain}";
+      example = "example.com";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    services.${name} = {
-      enable = true;
-      settings = {
-        server = {
-          bindaddress = cfg.address;
-          inherit (cfg) port;
+    services = {
+      ${name} = {
+        enable = true;
+        settings = {
+          server = {
+            bindaddress = cfg.address;
+            inherit (cfg) port;
+          };
+        };
+      };
+
+      caddy = lib.mkIf cfg.enableProxy {
+        virtualHosts."${cfg.url}" = {
+          extraConfig = lib.mkDefault ''
+            reverse_proxy http://${cfg.address}:${toString cfg.port}
+          '';
         };
       };
     };
